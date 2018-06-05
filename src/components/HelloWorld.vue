48851ff3ac6452d38f342db80dcd50a89e4a7d10
ohmygold <499452621@qq.com>
2018-06-06 07:13:28 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <div class="header">
+      <div class="logo">
+        <img src="./../assets/logo.png" alt="">
+      </div>
+      <div class="nav">
+          <router-link :to="{ name: '', params: {} }"><div>WORKS</div></router-link>
+          <router-link :to="{ name: '', params: {} }"><div>News</div></router-link>
+          <router-link :to="{ name: '', params: {} }"><div>Thinking</div></router-link>
+          <router-link :to="{ name: '', params: {} }"><div>About us</div></router-link>
+      </div>
+    </div>
+    <div class="banner">
+      <img src="./../assets/banner.jpg">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header, .banner{
+  width: 87.6%;
+  margin: 0 auto;
+  margin-top: 36px;
+}
+.header{
+  display: flex;
+  justify-content:space-between;
+}
+img{
+  width: 100%;
+  height: 100%;
+}
+.logo{
+width: 20%;
+}
+.nav{
+  display: flex;
+}
+.nav div{
+  padding: 20px 10px 0px 10px;
+}
+.nav div::after{
+  width: 1px;
+  background: #000000;
+}
+</style>
